3f16466ccfd2ecfbc098e42ea404fd84a25546ef
karta2341940 <a08487a@gmail.com>
2021-10-19 16:30:03 +0800

    Standard-color OK

--- js/navbar.js
+++ js/navbar.js
@@ -1,5 +1,17 @@
 document.addEventListener('DOMContentLoaded',function()
 {
+    document.body.onscroll=function()
+    {
+        if(window.scrollY > 200) 
+        {
+            document.querySelector("nav").classList.add("fixed-top");
+        }
+        else 
+        {
+            document.querySelector("nav").classList.remove("fixed-top");
+        }
+    }
+
 
     document.querySelector(".navbar-toggler").addEventListener("click",function (){
         
