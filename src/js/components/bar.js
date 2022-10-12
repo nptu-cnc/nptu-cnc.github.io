fe7e13b4e27891031ecacbb18c664d18372022c0
MX,Du <a08487a@gmail.com>
2022-10-12 16:21:41 +0800

    test

--- src/js/components/bar.js
+++ src/js/components/bar.js
@@ -6,7 +6,8 @@ export default {
     },
     template: `
         <div>
-            <h1>{{hello}}</h1>
+        <router-link to="/">Go to Home</router-link>
+        <router-link to="/about">Go to About</router-link>
         </div>
     `,
 
--- src/js/render.js
+++ src/js/render.js
@@ -1,14 +1,13 @@
 import { barComponent } from './components/index.js';
 import { homePage, aboutPage } from './pages/index.js';
-import { createApp } from "vue/dist/vue.esm-browser.js"
-import { createRouter, createWebHistory } from "vue-router/dist/vue-router.esm-browser"
+
 
 const routes = [
     { path: '/', component: homePage },
     { path: '/about', component: aboutPage }
 ]
-const router = createRouter({
-    history: createWebHistory(),
+const router = VueRouter.createRouter({
+    history: VueRouter.createWebHistory(),
     routes: routes
 });
 
@@ -31,7 +30,7 @@ const base = {
 
 
 
-createApp(base)
+Vue.createApp(base)
     .use(router)
     .mount("body");
 
