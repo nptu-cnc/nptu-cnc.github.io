b7f551f20a1d13d345f82c93876f24362dec660c
MX,Du <a08487a@gmail.com>
2022-11-04 16:44:40 +0800

    update

---
+++ nptu-cnc/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterView } from "vue-router";
+import TopNavBar from "./components/TopNavBar.vue";
+</script>
+
+<template>
+  <TopNavBar></TopNavBar>
+  <RouterView />
+</template>
+
+<style>
+
+</style>
